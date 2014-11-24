d063f2fd13fb8b2ee48e3c96fb8fc24ab76d809c
Becca Aaronson <becca.aaronson@gmail.com>
2014-11-24 16:58:27 -0600

    add pull out menu script/styles

--- txlege84/static/scripts/main.js
+++ txlege84/static/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('Hello!');
+(function () {
+  'use strict';
+
+  var querySelector = document.querySelector.bind(document);
+
+  var navdrawerContainer = querySelector('.navdrawer-container');
+  var body = document.body;
+  var appbarElement = querySelector('.app-bar');
+  var menuBtn = querySelector('.menu');
+  var main = querySelector('.main');
+  var icon = querySelector('.menu-icon');
+
+  function closeMenu() {
+    body.classList.remove('open');
+    appbarElement.classList.remove('open');
+    navdrawerContainer.classList.remove('open');
+  }
+
+  function toggleMenu() {
+    body.classList.toggle('open');
+    appbarElement.classList.toggle('open');
+    navdrawerContainer.classList.toggle('open');
+    navdrawerContainer.classList.add('opened');
+    icon.classList.toggle('x');
+  }
+
+  main.addEventListener('click', closeMenu);
+  menuBtn.addEventListener('click', toggleMenu);
+  navdrawerContainer.addEventListener('click', function (event) {
+    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
+      closeMenu();
+    }
+  });
+})();
