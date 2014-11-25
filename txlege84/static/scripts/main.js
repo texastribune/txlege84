5235ae5a6111be8dac79b6e7b695f45a1663fc65
Becca Aaronson <becca.aaronson@gmail.com>
2014-11-25 15:46:13 -0600

    add basic slick slider, no styles

--- txlege84/static/scripts/main.js
+++ txlege84/static/scripts/main.js
@@ -33,3 +33,14 @@
     }
   });
 })();
+
+$(document).ready(function() {
+  $('.single-item').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+  });
+});
+
