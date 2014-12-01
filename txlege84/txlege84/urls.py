c87165d67ea92291998375836c7d4d5a67e4f5e0
Becca Aaronson <becca.aaronson@gmail.com>
2014-12-01 10:53:01 -0600

    style topic list page

--- txlege84/txlege84/urls.py
+++ txlege84/txlege84/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='landing.html')),
     url(r'^topic-list-landing/$', TemplateView.as_view(template_name='topic-list-landing.html')),
+    url(r'^topic-list/$', TemplateView.as_view(template_name='topic-list.html')),
 
     # Examples:
     # url(r'^$', 'txlege84.views.home', name='home'),
