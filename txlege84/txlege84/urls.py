ca320d37db630329d83d531e19ad2792db14e641
Annie Daniel <adaniel@texastribune.org>
2014-12-09 14:48:21 -0600

    changed url's to reverse url's for bills on commitees.html and committees on bills.html

--- txlege84/txlege84/urls.py
+++ txlege84/txlege84/urls.py
@@ -12,9 +12,9 @@ urlpatterns = patterns(
     url(r'^$', TemplateView.as_view(template_name='landing.html')),
     url(r'^topic-list-landing/$', TemplateView.as_view(template_name='topic-list-landing.html')),
     url(r'^topic-list/$', TemplateView.as_view(template_name='topic-list.html')),
-    url(r'^84/bills/(?P<slug>[-\w]+)/$', BillDetail.as_view()),
+    url(r'^84/bills/(?P<slug>[-\w]+)/$', BillDetail.as_view(), name='bill-detail'),
     url(r'^legislator/$', TemplateView.as_view(template_name='legislator.html')),
-    url(r'^84/committees/(?P<chamber>[-\w]+)/(?P<slug>[-\w]+)/$', CommitteeDetail.as_view()),
+    url(r'^84/committees/(?P<chamber>[-\w]+)/(?P<slug>[-\w]+)/$', CommitteeDetail.as_view(), name='committee-detail'),
     url(r'^issue/$', TemplateView.as_view(template_name='issue.html')),
 
     # Examples:
