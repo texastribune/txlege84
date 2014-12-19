b422293d8735ed45de797b3d730f711a7177f379
Ryan Murphy <github@accounts.rdmurphy.org>
2014-12-19 14:33:10 -0600

    Reworking of urls.py

--- txlege84/txlege84/urls.py
+++ txlege84/txlege84/urls.py
@@ -5,25 +5,40 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 
 from bills.views import BillDetail
+from core.views import LandingDetail
 from committees.views import CommitteeDetail
 from legislators.views import LegislatorDetail
-from topics.views import TopicDetail
+from topics.views import IssueDetail, TopicDetail, TopicListDetail
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='landing.html')),
-    url(r'^topic-list-landing/$', TemplateView.as_view(template_name='pages/topic-list-landing')),
-    url(r'^84/topic-list/(?P<slug>[-\w]+)/$', TopicDetail.as_view(), name='pages/topic-list'),
-    url(r'^84/bills/(?P<slug>[-\w]+)/$', BillDetail.as_view(), name='bill-detail'),
-    url(r'^84/legislators/(?P<slug>[-\w]+)/$', LegislatorDetail.as_view(), name='legislator-detail'),
-    url(r'^84/committees/(?P<chamber>[-\w]+)/(?P<slug>[-\w]+)/$', CommitteeDetail.as_view(), name='committee-detail'),
-    url(r'^issue/$', TemplateView.as_view(template_name='pages/issue')),
+    url(r'^$', LandingDetail.as_view(), name='landing-detail'),
+    url(r'^84/hot-lists/$',
+        TopicDetail.as_view(), name='topic-detail'),
+    url(r'^84/hot-lists/(?P<slug>[-\w]+)/$',
+        TopicListDetail.as_view(), name='topic-list-detail'),
+    url(r'^84/issues/(?P<slug>[-\w]+)$',
+        IssueDetail.as_view(), name='issue-detail'),
+    url(r'^84/bills/(?P<slug>[-\w]+)/$',
+        BillDetail.as_view(), name='bill-detail'),
+    url(r'^84/legislators/(?P<slug>[-\w]+)/$',
+        LegislatorDetail.as_view(), name='legislator-detail'),
+    url(r'^84/committees/(?P<chamber>[-\w]+)/(?P<slug>[-\w]+)/$',
+        CommitteeDetail.as_view(), name='committee-detail'),
 
     # Examples:
     # url(r'^$', 'txlege84.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # FOR DEVELOPMENT:
+    url(r'^hot-list/$', TemplateView.as_view(
+        template_name='pages/topic-list-landing.html')),
+    url(r'^hot-list-detail/$', TemplateView.as_view(
+        template_name='pages/topic-list.html')),
+    url(r'^issue-detail/$', TemplateView.as_view(
+        template_name='pages/issue.html')),
 )
 
 # Uncomment the import above and next line to serve media files in development
