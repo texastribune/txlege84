d5acd362f1eaf62554f73093c9d62f8c5caf01b6
Ryan Murphy <github@accounts.rdmurphy.org>
2014-12-19 14:32:33 -0600

    Create TopicListDetail and IssueDetail views

--- txlege84/topics/views.py
+++ txlege84/topics/views.py
@@ -1,8 +1,18 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
-from topics.models import Topic
+from topics.models import Issue, Topic
+
+
+class TopicListDetail(ListView):
+    model = Topic
+    template_name = 'pages/topic-list-landing.html'
 
 
 class TopicDetail(DetailView):
     model = Topic
-    template_name = 'topic-list.html'
+    template_name = 'pages/topic-list.html'
+
+
+class IssueDetail(DetailView):
+    model = Issue
+    template_name = 'pages/issue.html'
