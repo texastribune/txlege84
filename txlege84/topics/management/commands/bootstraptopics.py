dac67367b9e3dea1a94a472c70b21122d3949ba3
Ryan Murphy <github@accounts.rdmurphy.org>
2014-12-21 17:02:18 -0600

    Add topic bootstrap command

---
+++ txlege84/topics/management/commands/bootstraptopics.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+
+from topics.models import Topic
+
+
+class Command(BaseCommand):
+    help = u'Bootstrap the topic lists in the database.'
+
+    def handle(self, *args, **kwargs):
+        self.load_topics()
+
+    def load_topics(self):
+        self.stdout.write(u'Loading hot list topics...')
+
+        topics = [
+            u'Budget & Taxes',
+            u'Business & Technology',
+            u'Civil Liberties',
+            u'Criminal Justice',
+            u'Energy & Environment',
+            u'Ethics',
+            u'Health & Human Services',
+            u'Higher Education',
+            u'Immigration & Border Security',
+            u'Public Education',
+            u'Transportation',
+        ]
+
+        for topic in topics:
+            Topic.objects.get_or_create(name=topic)
