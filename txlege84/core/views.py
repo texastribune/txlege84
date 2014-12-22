61d722ccd78184fe01db1bee984c535b3532f219
Annie Daniel <adaniel@texastribune.org>
2014-12-22 14:02:17 -0600

    first pass at views

--- txlege84/core/views.py
+++ txlege84/core/views.py
@@ -1,5 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from topics.models import Issue, Topic
 
 
 class LandingDetail(TemplateView):
+    model = Issue
     template_name = 'landing.html'
+
+
+class TopicListDetail(ListView):
+    model = Topic
+    template_name = 'landing.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TopicListDetail, self).get_context_data(**kwargs)
+        context['hot-list'] = Topic.objects.all()
+        return context
