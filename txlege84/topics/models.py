055cf35e4f9bc898b273455e183ab089d2dc0ff8
Ryan Murphy <github@accounts.rdmurphy.org>
2014-12-22 16:02:24 -0600

    Add curators, created_date, modified_date and active_text to Issue; Create IssueText

--- txlege84/topics/models.py
+++ txlege84/topics/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
 
@@ -13,6 +14,7 @@ PUBLICATION_CHOICES = (
 
 class Topic(models.Model):
     name = models.CharField(max_length=20, unique=True)
+    curators = models.ManyToManyField(settings.AUTH_USER_MODEL)
     slug = models.SlugField(null=True, blank=True)
 
     def __unicode__(self):
@@ -28,24 +30,36 @@ class Topic(models.Model):
 class Issue(models.Model):
     name = models.CharField(max_length=200)
     topic = models.ForeignKey(Topic, related_name='issues')
-    text = models.TextField()
+    active_text = models.OneToOneField('IssueText')
     slug = models.SlugField(null=True, blank=True)
     image = models.URLField(null=True, blank=True)
 
     order = models.IntegerField()
     status = models.CharField(
         max_length=1, choices=PUBLICATION_CHOICES, default=u'D')
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
 
     related_bills = models.ManyToManyField(Bill, related_name='issues')
 
     class Meta:
         ordering = ('order',)
 
+    def __unicode__(self):
+        return self.name
+
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = slugify(self.name)
 
         super(Issue, self).save(*args, **kwargs)
+
+
+class IssueText(models.Model):
+    associated_issue = models.ForeignKey(Issue, related_name='texts')
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
 
 
 class Stream(models.Model):
