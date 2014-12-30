5558cf1797225a08e71479343d257b51533a5e85
Ryan Murphy <github@accounts.rdmurphy.org>
2014-12-30 12:25:03 -0600

    Remove commented get_context_data

--- txlege84/core/views.py
+++ txlege84/core/views.py
@@ -6,8 +6,3 @@ from topics.models import Topic
 class LandingView(ListView):
     model = Topic
     template_name = 'landing.html'
-
-    # def get_context_data(self, **kwargs):
-    #     context = super(TopicList, self).get_context_data(**kwargs)
-    #     context['issueList'] = Topic.objects.issue.all()
-    #     return context
