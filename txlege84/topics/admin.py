ecd0c00766304f1e5b12e6067a846033a4ee36d5
Ryan Murphy <github@accounts.rdmurphy.org>
2015-01-03 15:52:59 -0600

    Move Issue ModelAdmin to new register syntax

--- txlege84/topics/admin.py
+++ txlege84/topics/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 from topics.models import Issue, StoryPointer, Stream, Topic
 
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(Topic)
-admin.site.register(Issue)
 admin.site.register(Stream)
 admin.site.register(StoryPointer)
