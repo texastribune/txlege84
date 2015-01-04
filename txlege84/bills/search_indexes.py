d4b1df4c934d9b40ed0c1b0e69f5df0254f0c11d
Ryan Murphy <github@accounts.rdmurphy.org>
2015-01-04 11:23:04 -0600

    Search index for bills

---
+++ txlege84/bills/search_indexes.py
@@ -0,0 +1,13 @@
+from bills.models import Bill
+
+from haystack import indexes
+
+
+class BillIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Bill
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
