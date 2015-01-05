755f0f7faaecf1ac0d7f7c05eb0f76d5af429863
Ryan Murphy <github@accounts.rdmurphy.org>
2015-01-05 16:00:06 -0600

    Add order field, add initial migrations

--- txlege84/explainers/models.py
+++ txlege84/explainers/models.py
@@ -5,9 +5,13 @@ class Explainer(models.Model):
     title = models.CharField(max_length=200)
     youtube_id = models.CharField(max_length=11)
     text = models.TextField()
+    order = models.PositiveIntegerField(default=0)
 
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('order',)
 
     def __unicode__(self):
         return self.title
