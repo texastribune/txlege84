41e1484982f51a51422a92589bcbc33cb9288ce0
Ryan Murphy <github@accounts.rdmurphy.org>
2015-01-13 01:16:45 -0600

    Slight shift

--- txlege84/legislators/mixins.py
+++ txlege84/legislators/mixins.py
@@ -4,5 +4,8 @@ from legislators.models import Legislator
 class AllLegislatorsMixin(object):
     def get_context_data(self, **kwargs):
         context = super(AllLegislatorsMixin, self).get_context_data(**kwargs)
-        context['legislator_list'] = Legislator.objects.all()
+        context['rep_list'] = Legislator.objects.filter(
+            chamber__name='Texas House').order_by('last_name')
+        context['sen_list'] = Legislator.objects.filter(
+            chamber__name='Texas Senate').order_by('last_name')
         return context
