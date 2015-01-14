b3318b043cff180611ecd9bd690e00dd5c05e260
Ryan Murphy <github@accounts.rdmurphy.org>
2015-01-13 23:30:42 -0800

    Support for tracking convening times

--- txlege84/legislators/mixins.py
+++ txlege84/legislators/mixins.py
@@ -1,4 +1,4 @@
-from legislators.models import Legislator
+from legislators.models import Chamber, Legislator
 
 
 class AllLegislatorsMixin(object):
@@ -8,4 +8,12 @@ class AllLegislatorsMixin(object):
             chamber__name='Texas House').order_by('last_name').active()
         context['sen_list'] = Legislator.objects.filter(
             chamber__name='Texas Senate').order_by('last_name').active()
+        return context
+
+
+class ChambersMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(ChambersMixin, self).get_context_data(**kwargs)
+        context['house'] = Chamber.objects.get(name='Texas House')
+        context['senate'] = Chamber.objects.get(name='Texas Senate')
         return context
