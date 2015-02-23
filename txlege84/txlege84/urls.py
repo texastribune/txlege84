70b80afab1f7c7678684c816f1cc93ebca4c98c2
Becca Aaronson <becca.aaronson@gmail.com>
2015-02-23 15:23:54 -0600

    create redirects for hot-lists > topics

--- txlege84/txlege84/urls.py
+++ txlege84/txlege84/urls.py
@@ -16,11 +16,11 @@ from topics.views import IssueDetail, TopicDetail, TopicListDetail
 urlpatterns = patterns(
     '',
     url(r'^$', LandingView.as_view(), name='landing-view'),
-    url(r'^hot-lists/$',
+    url(r'^topic/$',
         TopicListDetail.as_view(), name='topic-list-detail'),
-    url(r'^hot-lists/(?P<slug>[-\w]+)/$',
+    url(r'^topic/(?P<slug>[-\w]+)/$',
         TopicDetail.as_view(), name='topic-detail'),
-    url(r'^hot-lists/(?P<hot_list_slug>[-\w]+)/(?P<slug>[-\w]+)/$',
+    url(r'^topic/(?P<hot_list_slug>[-\w]+)/(?P<slug>[-\w]+)/$',
         IssueDetail.as_view(), name='issue-detail'),
     url(r'^84/bills/(?P<slug>[-\w]+)/$',
         BillDetail.as_view(), name='bill-detail'),
@@ -32,15 +32,21 @@ urlpatterns = patterns(
         CommitteeDetail.as_view(), name='committee-detail'),
     url(r'^how-session-works/$',
         ExplainerListDetail.as_view(), name='explainer-list-detail'),
-
     url(r'^livestream/$',
         LegeStreamDetail.as_view(), name='legestream'),
 
     url(r'^search/bills/', BillSearchJson.as_view(), name='bill-search'),
 
     # Redirects
+    url(r'^hot-lists/$',
+        RedirectView.as_view(url='topic')),
+    url(r'^hot-lists/(?P<slug>[-\w]+)/$',
+        RedirectView.as_view(url='../../topic/%(slug)s/')),
+    url(r'^hot-lists/(?P<hot_list_slug>[-\w]+)/(?P<slug>[-\w]+)/$',
+        RedirectView.as_view(url='../../../topic/%(hot_list_slug)s/%(slug)s/')),
+
     url(r'^legestream/$',
-        RedirectView.as_view(url='../livestream/'), name='legestream'),
+        RedirectView.as_view(url='../livestream/')),
     url(r'^texplainers/$',
         RedirectView.as_view(url='../how-session-works/')),
 
