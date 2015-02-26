e50d2f6ce830982116cc58c363c0ab5d44c91df2
Ryan Murphy <github@accounts.rdmurphy.org>
2015-02-26 11:34:21 -0600

    Clean up URL

--- txlege84/txlege84/urls.py
+++ txlege84/txlege84/urls.py
@@ -34,7 +34,7 @@ urlpatterns = patterns(
     url(r'^legestream/$',
         LegeStreamDetail.as_view(), name='legestream'),
     url(r'^search/bills/', BillSearchJson.as_view(), name='bill-search'),
-    url(r'^find-bills/', BillSearchView.as_view(), name='find-bills'),
+    url(r'^find-bills/$', BillSearchView.as_view(), name='find-bills'),
 
     # Examples:
     # url(r'^$', 'txlege84.views.home', name='home'),
