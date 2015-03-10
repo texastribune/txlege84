d910b5c9028b34417ec7fdab7bf7b3fd80efd28a
Ryan Murphy <github@accounts.rdmurphy.org>
2015-03-10 17:19:34 -0500

    Code style pickiness

--- txlege84/legislators/views.py
+++ txlege84/legislators/views.py
@@ -1,13 +1,15 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView, ListView
 
 from bills.mixins import AllSubjectsMixin
 from core.mixins import ConveneTimeMixin
 from legislators.mixins import AllLegislatorsMixin
 from legislators.models import Legislator
 
+
 class LegislatorList(AllLegislatorsMixin, ConveneTimeMixin, ListView):
     model = Legislator
     template_name = 'pages/legislator-landing.html'
+
 
 class LegislatorDetail(AllSubjectsMixin, AllLegislatorsMixin,
                        ConveneTimeMixin, DetailView):
