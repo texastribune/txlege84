4256289d5f455252769fbc410be979c595968af3
Ryan Murphy <github@accounts.rdmurphy.org>
2015-03-11 17:24:18 -0500

    More bulletproof to ensure only one Topic is ever featured

--- txlege84/core/views.py
+++ txlege84/core/views.py
@@ -3,10 +3,10 @@ from django.views.generic import ListView
 from core.mixins import ConveneTimeMixin
 from topics.mixins import FeaturedTopicMixin
 
-from topics.models import Topic
+from topics.models import FeaturedTopic, Topic
 
 
 class LandingView(FeaturedTopicMixin, ConveneTimeMixin, ListView):
     # only returns the one not considered a featured topic
-    queryset = Topic.objects.filter(featured_topic__isnull=True)
+    queryset = Topic.objects.exclude(id=FeaturedTopic.objects.first().topic.id)
     template_name = 'landing.html'
