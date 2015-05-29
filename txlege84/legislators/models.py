181aaea0ca4d7ae9483f38526e9dbd8655953191
Annie Daniel <adaniel@texastribune.org>
2015-05-29 14:13:42 -0500

    cleaned up models and added loser bill table

--- txlege84/legislators/models.py
+++ txlege84/legislators/models.py
@@ -142,21 +142,3 @@ class Legislator(models.Model):
     @property
     def authored_bills(self):
         return self.sponsorships.filter(role='author')
-
-
-    # @property
-    # def new_laws(self):
-    #     return self.sponsorships.filter(
-    #         role='author', bill__became_law__isnull=False)
-
-    # @property
-    # def vetoed_laws(self):
-    #     return self.sponsorships.filter(
-    #         role='author', bill__vetoed__isnull=False)
-
-    # @property
-    # def passed_both(self):
-    #     return self.sponsorships.filter(
-    #         role='author', bill__passed_se)
-
-
